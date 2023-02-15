df54e0ae6609e5ef3690d940d9d06f1846e29444
Natalia <nata130692@mail.ru>
2023-02-15 18:08:26 +0300

    save data in localStorage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from 'react'
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
-import mock from './mock.json'
 
 function App() {
-  const [tasks, setTasks] = useState(mock)
+  const initialState = JSON.parse(window.localStorage.getItem('tasks')) || []
+  const [tasks, setTasks] = useState(initialState)
+
+	useEffect(() => {
+		window.localStorage.setItem('tasks', JSON.stringify(tasks))
+	}, [tasks])
 
   return (
     <div className='wrapper'>
